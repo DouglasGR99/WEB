e357ad43d64e5a021a7b44225eb6492dbdac7ceb
Douglas Garcia <douglasgovmad@gmail.com>
2023-03-28 21:26:02 -0300

    modificacao dos nomes das pastas

---
+++ aula 2(tabela e calculadora)/calculadora.php
@@ -0,0 +1,19 @@
+<?php
+    $v1 = "";
+    $v2 = "";
+    $result = "";
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calcular soma</title>
+</head>
+<body>
+    
+</body>
+</html>
