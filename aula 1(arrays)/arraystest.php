c359ddeb2213a1857f63f078916750aff7ab285c
Douglas Garcia <douglasgovmad@gmail.com>
2023-03-29 15:21:01 -0300

    arraysTest update

--- aula 1(arrays)/arraystest.php
+++ aula 1(arrays)/arraystest.php
@@ -1,12 +1,21 @@
+<?php
+    $carro = array("volkswagen","nissan","fiat","alfaromeo","lamborghini");
+?>
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Php arrays</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 <body>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?> 
-
+<main>
+    <h1>My first PHP page</h1>
+    <?php for ($i=0; $i < 5 ; $i++) { ?>
+    <?php echo ("O meu $carro[$i] corre, corre e corre."); ?>  
+    <br>
+    <?php } ?> 
+</main>
 </body>
 </html>
