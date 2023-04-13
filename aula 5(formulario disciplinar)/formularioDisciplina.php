536b8af037f76664ac1b16e1c138ad8a7f1efa61
Douglas Garcia <douglasgovmad@gmail.com>
2023-04-13 01:56:15 -0300

    tabela criada/otimizada e css aplicado

--- aula 5(formulario disciplinar)/formularioDisciplina.php
+++ aula 5(formulario disciplinar)/formularioDisciplina.php
@@ -2,42 +2,43 @@
     $nome = "";
     $sigla = "";
     $carga = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { // se o método de requisição for POST (envio de dados) então faça:
         $nome = $_POST["nome"];
         $sigla = $_POST["sigla"];
         $carga = $_POST["carga"];
         $linha = "";
-        echo "nome " . $nome . " sigla: " . $sigla . " carga: " . $carga;
-        if (!file_exists( "disciplinas.txt")) {
+        // echo "nome " . $nome . " sigla: " . $sigla . " carga: " . $carga;
+        if (!file_exists( "disciplinas.txt")) { // se o arquivo não existir então faça:
             $arcDisc = fopen( "disciplinas.txt",  "a") or die("erro ao criar arquivo");
             $linha = "nome;sigla;carga\n";
-            fwrite($arcDisc, $linha);
+            fwrite($arcDisc, $linha); // fwrite escreve uma string no arquivo
             fclose($arcDisc);
         }
-        $arcDisc = fopen( "disciplinas.txt",  "a") or die("erro ao criar arquivo");
-        $linha = $nome . ";" . $sigla . ";" . $carga . "\n";
-        fwrite($arcDisc, $linha);
-        fclose($arcDisc);
+        $arcDisc = fopen( "disciplinas.txt",  "a") or die("erro ao criar arquivo"); // abre o arquivo para adicionar
+        $linha = $nome . ";" . $sigla . ";" . $carga . "\n"; // cria a linha a ser adicionada
+        fwrite($arcDisc, $linha); // escreve a linha no arquivo
+        fclose($arcDisc);   // fecha o arquivo
     }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fornulario disciplina php</title>
 </head>
 <body>
-    <h1>Formulario Disciplina</h1>
-    <form action="formularioDisciplina.php" method="POST">
-        Nome: <input type="text" name="nome">
-        <br>
-        Sigla: <input type="text" name="sigla">
-        <br>
-        Carga H: <input type="text" name="carga">
-        <br>
-        <input type="submit" value="Criar nova disciplina">
-    </form>
+    <main class="caixas">
+        <h1>Formulario Disciplinar</h1>
+        <form action="formularioDisciplina.php" method="POST">
+            <h3>Nome: <input type="text" name="nome"></h3>
+            <h3>Sigla: <input type="text" name="sigla"></h3>
+            <h3>Carga H: <input type="text" name="carga"></h3>
+            <br>
+            <input class="botaoBonito" type="submit" value="Criar nova disciplina"> 
+        </form>
+    </main>
 </body>
 </html>
--- aula 5(formulario disciplinar)/reportDisciplina.php
+++ aula 5(formulario disciplinar)/reportDisciplina.php
@@ -1,37 +1,47 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar disciplina php</title>
 </head>
 <body>
-    <h1>Listar Disciplina</h1>
-    <?php
-        $arcDisc = fopen("disciplinas.txt" ,  "r") or die ("erro ao ler arquivo");
-        // echo fgets($arcDisc);
-        // echo "<br>";
-        $cabecalho = explode( ";" , fgets($arcDisc));
-        // echo $cabecalho[0];
-        echo "<br>";
-        echo fgets($arcDisc);
-        //ler fgets ate end of file (eof retornar true)
-        while (!feof($arcDisc)) {
-            echo fgets($arcDisc);
-            echo "<br>";
-        }
-    ?>
-    <br>
-    <table style="border: 1px solid">
-        <tr>
-            <th style="border: 1px solid"><?php echo $cabecalho[0] ?></th>
-            <th style="border: 1px solid"><?php echo $cabecalho[1] ?></th>
-            <th style="border: 1px solid"><?php echo $cabecalho[2] ?></th>
-        </tr>
-        <tr>
-            <td></td>
-        </tr>
-    </table>
+    <main class="caixas">
+        <h1>Listar Disciplinas</h1>
+        <?php
+            $arcDisc = fopen("disciplinas.txt", "r") or die("Erro ao ler arquivo");
+            // r = read
+            $cabecalho = explode(";", fgets($arcDisc));
+            // fgets lê uma linha do arquivo e retorna uma string
+        ?>
+        <br>
+        <table class='tabela'>
+            <tr>
+                <th class='tabela'><?php echo $cabecalho[0] ?></th>
+                <th class='tabela'><?php echo $cabecalho[1] ?></th>
+                <th class='tabela'><?php echo $cabecalho[2] ?></th>
+            </tr>
+            <?php
+            while (!feof($arcDisc)) {
+                // enquanto não chegar no final do arquivo
+                $linha = fgets($arcDisc);
+                if (!empty($linha)) {
+                    // se a linha não estiver vazia
+                    $dados = explode(";", $linha);
+                    // explode separa a string em um array de strings
+                    echo "<tr>";
+                    for ($i = 0; $i < count($dados); $i++) {
+                        // count conta o número de elementos de um array
+                        echo "<td class='tabela'>" . $dados[$i] . "</td>";
+                    }
+                    echo "</tr>";
+                }
+            }
+            ?>
+        </table>
+        <?php fclose($arcDisc); ?>
+    </main>
 </body>
 </html>
