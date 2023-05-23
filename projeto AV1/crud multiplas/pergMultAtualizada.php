8229b108cbdfbe2be67a40f14910302fcd15408b
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-05-23 01:11:50 -0300

    AtualizaPergOptativa finalmente funcional!!!!

--- projeto AV1/crud multiplas/pergMultAtualizada.php
+++ projeto AV1/crud multiplas/pergMultAtualizada.php
@@ -1,21 +1,41 @@
 <?php
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") { // se o método de requisição for POST (envio de dados) então faça:
-    //variaveis para atualizar
-    $nome = $_POST["nome"];
-    $cpf = $_POST["cpf"];
-    $matriculaSelec = $_SESSION["matricula"];
-    $DTingresso = $_POST["DTingresso"];
+
+    $questaoID = $_SESSION["questaoID"];
+    //variaveis para atualizar:
+    $pergunta = $_POST["pergunta"];
+
+    $respostas = array(
+        $_POST["resposta1"],
+        $_POST["resposta2"],
+        $_POST["resposta3"],
+        $_POST["resposta4"],
+        $_POST["resposta5"]
+    );
+
+    $gabaritos = array(
+        $_POST["gabarito1"],
+        $_POST["gabarito2"],
+        $_POST["gabarito3"],
+        $_POST["gabarito4"],
+        $_POST["gabarito5"]
+    );
 
     //variaveis de leitura de linha
-    $linhas = file("alunos.txt"); // lê todas as linhas do arquivo e armazena em um array
-    $linha_nova = $nome . ";" . $cpf . ";" . $matriculaSelec . ";" . $DTingresso . "\n"; // cria a linha a ser adicionada
+    $linhas = file("../perguntas.txt"); // lê todas as linhas do arquivo e armazena em um array
     $indice_atualizar = -1; // variável para armazenar o índice da linha a ser atualizada
+
+    $linha_nova = "Pergunta optativa: " . $pergunta . ";" . $questaoID . "\n";
+    $opcoes = array();
+    for($i = 0; $i < 5; $i++) {
+        $opcoes[$i] = "(" . chr(97 + $i) . ") " . $respostas[$i] . ";" . $gabaritos[$i] . "\n";
+    }
 
     // Percorre todas as linhas do arquivo com o foreach
     foreach($linhas as $indice => $linha) {
         $dados = explode(";", $linha);
-        if($dados[2] == $matriculaSelec) { // Verifica se a linha contém a matrícula fornecida
+        if($dados[1] == $questaoID) { // Verifica se a linha contém o ID fornecido
             $indice_atualizar = $indice;
             break;
         }
@@ -23,16 +43,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // se o método de requisição for 
 
     if($indice_atualizar !== -1) { // Se a linha for encontrada
         $linhas[$indice_atualizar] = $linha_nova; // Substitui a linha antiga pela nova
-        $arquivo = fopen("alunos.txt", "w"); // Abre o arquivo para escrita
+        for($i = 0; $i < 5; $i++) { // Substitui as opções
+            $linhas[$indice_atualizar + $i + 1] = $opcoes[$i];
+        }
+        $arquivo = fopen("../perguntas.txt", "w"); // Abre o arquivo para escrita
         fwrite($arquivo, implode("", $linhas)); // Escreve as linhas restantes no arquivo
         fclose($arquivo);
         echo '<script type="text/javascript">
-                alert("Aluno modificado com sucesso!");
+                alert("Questão modificada com sucesso!");
               </script>';
     } else {
         echo
         '<script type="text/javascript">
-                alert("Erro ao modificar Aluno!");
+                alert("Erro ao modificar questão!");
             </script>';
     }
 }
@@ -42,34 +65,62 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // se o método de requisição for 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atualizar aluno</title>
+    <title>Pergunta Optativa Atualizada</title>
 </head>
 <body>
 <header class="caixas">
-    <h1>Atualizar Aluno</h1>
+    <h1>Pergunta Optativa Atualizada</h1>
     <nav>
-        <a href="pagInicial.html"><button class="botaoBonito pagInicio">Voltar a pag Inicial</button></a>
-        <a href="reportAluno.php"><button class="botaoBonito pagRead">Listar Alunos</button></a>
-        <a href="atualizarAluno.php"><button class="botaoBonito pagUpdate">Voltar a busca</button></a>
+        <a href="../pagInicial.html"><button class="botaoBonito pagInicio">Voltar a pag Inicial</button></a>
+        <a href="../reportPerg.php"><button class="botaoBonito pagRead">Listar Perguntas</button></a>
     </nav>
 </header>
 
 <main class="caixas">
-    <form action="alunoAtualizado.php" method="POST">
-        Novo Nome: <label>
-            <input type="text" name="nome">
+    <form action="pergMultAtualizada.php" method="POST">
+        Nova Pergunta: <label>
+            <input type="text" name="pergunta">
         </label>
-        Novo CPF: <label>
-            <input type="text" name="cpf">
+        <br>
+        Opção 1(atualizada): <label>
+            <input type="text" name="resposta1">
         </label>
-        Nova data de ingresso: <label>
-            <input type="text" name="DTingresso">
+        Gabarito atualizado (opc 1) (sim/não): <label>
+            <input type="text" name="gabarito1">
         </label>
-        <input type="submit" name="modificar" value="Modificar Aluno" class="botaoBonito pagUpdate">
+
+        Opção 2(atualizada): <label>
+            <input type="text" name="resposta2">
+        </label>
+        Gabarito atualizado (opc 2) (sim/não): <label>
+            <input type="text" name="gabarito2">
+        </label>
+
+        Opção 3(atualizada): <label>
+            <input type="text" name="resposta3">
+        </label>
+        Gabarito atualizado (opc 3) (sim/não): <label>
+            <input type="text" name="gabarito3">
+        </label>
+
+        Opção 4(atualizada): <label>
+            <input type="text" name="resposta4">
+        </label>
+        Gabarito atualizado (opc 4) (sim/não): <label>
+            <input type="text" name="gabarito4">
+        </label>
+
+        Opção 5(atualizada): <label>
+            <input type="text" name="resposta5">
+        </label>
+        Gabarito atualizado (opc 5) (sim/não): <label>
+            <input type="text" name="gabarito5">
+        </label>
+        <input type="submit" name="modificar" value="Modificar questão" class="botaoBonito pagUpdate">
     </form>
 </main>
 </body>
