2b84e4e19713317333870f09628d088e1dd48ffb
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-06-05 15:01:57 -0300

    disc update completo, crud discursivas

--- projeto AV1/CRUD Discursivas/DISCupdate.php
+++ projeto AV1/CRUD Discursivas/DISCupdate.php
@@ -8,17 +8,44 @@
     <title>Editar Questão Discursiva (PHP JSON)</title>
 </head>
 <body>
+<?php
+$indice = $_GET['indice'];
+
+$dados = file_get_contents('ARQdiscursivas.json');
+$data_array = json_decode($dados);
+
+if(isset($_POST['salvar'])){
+    $input = array(
+        'id' => $_POST['id'],
+        'pergunta' => $_POST['pergunta'],
+        'resposta' => $_POST['resposta']
+    );
+
+    $data_array[$indice] = $input;
+
+    $dados = json_encode($data_array, JSON_PRETTY_PRINT);
+    file_put_contents('ARQdiscursivas.json', $dados);
+
+    header('location: DISCindex.php');
+}
+?>
 <main class="caixas">
     <h1>Editar Questão Discursiva (PHP JSON)</h1>
     <nav>
         <a href="DISCindex.php"><button class="botaoBonito pagInicio">Voltar a pag Inicial</button></a>
     </nav>
     <br>
-    <form action="DISCupdate.php" method="post">
-        <label for="questaoID">Informe o ID da pergunta discursiva a ser atualizada:</label>
-        <input type="text" id="questaoID" name="questaoID">
-        <br><br>
-        <input type="submit" value="Buscar" name="buscarPERG" class="botaoBonito pagUpdate">
+    <form method="POST">
+        Índice: <label>
+            <input type="text" id="id" name="id">
+        </label>
+        Pergunta: <label>
+            <input type="text" id="pergunta" name="pergunta">
+        </label>
+        Resposta: <label>
+            <input type="text" id="resposta" name="resposta">
+        </label>
+        <input type="submit" name="salvar" value="Salvar" class="botaoBonito botaoArquivo">
     </form>
 </main>
 </body>
